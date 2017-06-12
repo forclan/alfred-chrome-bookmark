32e4b9d539568c27afec051baad517aece66c02e
hanxipeng <hanxipeng@bytedance.com>
2017-06-12 16:02:11 +0800

    alfred 可以显示的list需要包含title/subtitile/arg字段
    而且结果需要是字符串
    
    Change-Id: I9cf8eeb5ee3996749ba948845488ad3bbfbddd22

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const getPinYinBookmark = require('./bookmark');
 const debug = require('debug')('index.js');
-const alfy = require('alfy');
+// const alfy = require('alfy');
 const Fuse = require('fuse.js');
 
 const bookmarks = getPinYinBookmark();
 const fuse = new Fuse(bookmarks, {
   keys: ['pinyin', 'url']
-})
+});
+debug(fuse);
 const inputStr = process.argv[2];
 
-alfy.output(fuse(inputStr));
+function formatBookmark (bookmark) {
+  return {
+    title: bookmark.name,
+    subtitle: bookmark.url,
+    arg: bookmark.url
+  };
+}
+const generateOutputArray = input => fuse.search(input).map(formatBookmark);
+
+const generateOutput = input => {
+  return JSON.stringify({
+    items: generateOutputArray(input)
+  });
+}
+// alfy.output(fuse(inputStr));
+console.log(generateOutput(inputStr));
+
+
+// console.log(JSON.stringify(t2));
+// alfy.output(generateOutput(inputStr));
